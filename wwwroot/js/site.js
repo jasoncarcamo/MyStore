a6342959e85411eab533af16274f8fe30a640ac4
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-10-15 22:51:47 -0400

    First test deploy

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,17 @@
 const endpoint = 'api/Employees';
 
 
+document.getElementById("submit-button").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    fetch(endpoint, {
+        mode: "no-cors",
+        redirect: "follow",
+        headers: {
+            'content-type': "application/json"
+        }
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+})
